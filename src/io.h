62ea43071b2007beb62ea19215f759f11fa5db00
Rain <33805016+Rain1950@users.noreply.github.com>
2025-03-21 20:58:29 +0100

    Add reading file to memory and basic parsing

--- src/io.h
+++ src/io.h
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <ctype.h>
 
 void help();
 int parse_args(int argc, char** argv, char **input_file, char **output_file, int *parts, int *margin, char **format);
 
+void read_file(char* filename, int* max_count, char* indexes, char* first_nodes_indexes, char* groups, char* first_group_indexes);
+void buffer_to_int_array (char* src, int* dest ,int size,int* copied_count);
