0f35037dee2bcb4d74ea6242d118326d2591f6a4
mejxe <mlaskowski707@gmail.com>
2025-03-26 20:17:04 +0100

    Add graph and algorithm related structs headers

--- src/algorytm.h
+++ src/algorytm.h
@@ -1,4 +1,23 @@
 #include "graph.h"
+
+// Struktura kubełkowa dla FM (dla możliwie najszybszego dostępu do elementów)
+typedef struct {
+    int max_gain;       // Maksymalny możliwy zysk
+    int min_gain;       // Minimalny możliwy zysk
+    int* bucket_sizes;  
+    int** buckets;      // Tablica kubełków
+    int* vertex_bucket; // Indeks kubełka dla każdego wierzchołka
+} BucketStructure;
+
+// inicjalizacja struktury kubełkowej
+BucketStructure* initialize_buckets(int num_vertices, int max_degree);
+
+// dodawanie do kubełka
+void add_to_bucket(BucketStructure* bs, int vertex, int gain);
+
+// usuwanie z kubełka
+void remove_from_bucket(BucketStructure* bs, int vertex, int gain);
+
 // implementacja algorytmu FM
 //
 // inicjalizacja wierzchołków z informacjami potrzebnymi do korzystania z algorytmu FM
@@ -9,6 +28,9 @@ int calculate_initial_gain(Graph* graph, Vertex* vertices, int v);
 
 // obliczanie zysku wynikającego z przesunięcia wierzchołka
 int calculate_move_gain(Graph* graph, Vertex* vertices, int v, int target_partition);
+
+// znalezienie wierzchołka o najwyższym zysku
+int get_max_gain_vertex(BucketStructure* bs);
 
 // główna implementacja algorytmu
 Graph* fm_partition(Graph* graf, int parts, int margin);
--- src/graph.h
+++ src/graph.h
@@ -19,6 +19,7 @@ typedef struct {
     int locked; // 0/1
 } Vertex;
 
+
 // funkcja inicjujaca graf
 Graph* create_graph(int max_count, int* indexes, int* first_nodes_indexes, int* groups, int* first_group_indexes, int first_node_indexes_count);
 
@@ -28,16 +29,6 @@ void add_edge(Graph* graph, Node src, Node dest);
 // printowanie graphu na potrzeby debuggowania
 void display_graph(Graph* graph, int size_x, int size_y);
 
-// implementacja algorytmu FM
-//
-// inicjalizacja wierzchołków z informacjami potrzebnymi do korzystania z algorytmu FM
-void initialize_vertices(Graph* graph, Vertex* vertices, int num_partitions);
+// Funkcja zwalniająca pamięć zajmowaną przez graf
+void free_graph(Graph* graph);
 
-// obliczanie zysków początkowych dla wierzchołka grafu
-int calculate_initial_gain(Graph* graph, Vertex* vertices, int v);
-
-// obliczanie zysku wynikającego z przesunięcia wierzchołka
-int calculate_move_gain(Graph* graph, Vertex* vertices, int v, int target_partition);
-
-// główna implementacja algorytmu
-Graph* fm_partition(Graph* graf, int parts, int margin);
